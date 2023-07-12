5e51f43be50b51c9641e937bb919b44886cdb924
Amirali Amanipour <arshia.ala80@gmail.com>
2023-07-12 20:49:59 +0330

    fixed the attributes

--- components/modules/Attributes.js
+++ components/modules/Attributes.js
@@ -21,7 +21,8 @@ function Attributes() {
         </div>
 
         <div 
-        className='shadow-md shadow-gray-400 xs:w-full md:w-40
+        className='shadow-md shadow-gray-400 
+        xs:w-full md:w-40 px-12 py-8
         rounded flex flex-col justify-center items-center'>
         
           <Food />
@@ -29,7 +30,8 @@ function Attributes() {
         </div>
 
         <div
-        className='shadow-md shadow-gray-400 xs:w-full md:w-40
+        className='shadow-md shadow-gray-400 
+        xs:w-full md:w-40 px-12 py-8
         rounded flex flex-col justify-center items-center'
         >
           <Choice />
@@ -37,7 +39,8 @@ function Attributes() {
         </div>
 
         <div
-        className='shadow-md shadow-gray-400 xs:w-full md:w-40
+        className='shadow-md shadow-gray-400 
+        xs:w-full md:w-40 px-12 py-8
         rounded flex flex-col justify-center items-center '
         >
           <Clock />
