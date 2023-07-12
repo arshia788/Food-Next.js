3a706f21b8b7c2a6291ca1619c18bf6c04ff7356
Amirali Amanipour <arshia.ala80@gmail.com>
2023-07-13 01:19:38 +0330

    working on dynamic data of detail page

--- pages/menu/[id].js
+++ pages/menu/[id].js
@@ -1,38 +1,49 @@
+import { useRouter } from 'next/router'
 import React from 'react'
 
 export default function Details() {
-  return (
-    <div>Details</div>
-  )
+
+    const router= useRouter();
+
+    if(router.isFallback){
+        return <h2>Is-Loading...</h2>
+    }
+    return (
+        <div></div>
+    )
 }
 
-export async function getStaticPaths(){
+export async function getStaticPaths() {
 
-    const res= await fetch('http://localhost:4000/data')
-    const data= await res.json()
-    const newData= data.slice(0, 4);
-    const paths= newData.map(item=> ({params:{id:item.id.toString()}}) )
+    const res = await fetch('http://localhost:4000/data')
+    const data = await res.json()
+    const newData = data.slice(0, 4);
+    const paths = newData.map(item => ({ params: { id: item.id.toString() } }))
 
-    return{
+    return {
         paths,
-        fallback:true
+        fallback: true
     }
 }
 
-export async function getStaticProps(context){
-    
-    const {params}= context
-    console.log(params);
-    
-    const res= await fetch(`http://localhost:4000/data/${params.id}`)
-    const data=  await res.json()
-    console.log(data);
+export async function getStaticProps(context) {
 
-    return{
-        props:{
+    const { params } = context
+    console.log(params);
+
+    const res = await fetch(`http://localhost:4000/data/${params.id}`)
+    const data = await res.json()
+
+    if (!data.id) {
+        return {
+            notFound: true
+        }
+    }
+
+    return {
+        props: {
             data
         },
     }
 }
 
- 
