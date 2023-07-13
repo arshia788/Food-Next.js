7dcc18f469cb9639975cfa0cd62df080f164e260
Amirali Amanipour <arshia.ala80@gmail.com>
2023-07-13 16:18:06 +0330

    added query to url

--- components/templates/CategoriesPage.js
+++ components/templates/CategoriesPage.js
@@ -1,37 +1,62 @@
+import { useRouter } from "next/router"
+import { useState } from "react"
 
 export default function CategoriesPage() {
-  return (
-    <div className="my-8">
-        <h2 
-        className="font-bold text-xl border-b-2 border-green-500 w-fit"
-        >Categories</h2>
 
-        <div className="mt-8">
-            <div className="flex items-center justify-between">
-                <select className="rounded  text-green-500 p-1
-                
-                ">
-                    <option>Difficulty</option>
-                    <option>Easy</option>
-                    <option>Medium</option>
-                    <option>Hard</option>
-                </select>
+    const router= useRouter();
+    const [query, setQuery]= useState({
+        difficulty:'', time:''
+    })
 
-                <select className="rounded  text-green-500 p-1">
-                    <option>Cooking Time</option>
-                    <option>More than 30min</option>
-                    <option>Less tan 30min</option>
+    const changeHandler=(event)=>{
+        setQuery({...query, [event.target.name]:event.target.value})
+    }
 
-                </select>
+    const searchHandler=()=>{
+        router.push(
 
-                <button 
-                className="bg-green-500 text-white rounded px-4 py-1"
-                >Search</button>
+            {pathname:'/categories', query}
+        )
+    }
+
+    return (
+        <div className="my-8">
+            <h2
+                className="font-bold text-xl border-b-2 border-green-500 w-fit"
+            >Categories</h2>
+
+            <div className="mt-8">
+                <div className="flex items-center justify-between">
+                    <select 
+                    value={query.difficulty}
+                    name="difficulty"
+                    onChange={changeHandler}
+                    className="rounded  text-green-500 p-1">
+                        <option value="">Difficulty</option>
+                        <option value="Easy">Easy</option>
+                        <option value="Medium">Medium</option>
+                        <option value="Hard">Hard</option>
+                    </select>
+
+                    <select 
+                    value={query.time}
+                    name="time"
+                    onChange={changeHandler}
+                    className="rounded  text-green-500 p-1">
+                        <option value="">Cooking Time</option>
+                        <option value="more">More than 30min</option>
+                        <option value="less">Less tan 30min</option>
+
+                    </select>
+
+                    <button
+                        onClick={searchHandler}
+                        className="bg-green-500 text-white rounded px-4 py-1"
+                    >Search</button>
+                </div>
             </div>
-        </div>
 
-    </div>
-  )
+        </div>
+    )
 }
 
- 
